82f102038e6fd1d9ed850254117cd375f12559fd
oc34n <ocean.wrng@gmail.com>
2023-05-05 12:24:57 -0400

    Added MSPaint and refactored classNames for browser app

--- main.js
+++ main.js
@@ -1,58 +1,100 @@
-//OPEN CLOSE & TOGGLE APP CONTAINER
-
-function closeApp() {
-    app.classList.add('hidden');
-    closeBrowserTab();
-}
-
-function openApp() {
-    app.classList.remove('hidden');
-}
-
-function toggleApp() {
-    if(browserTab.classList.contains('show')) {
-        app.classList.toggle('hidden')    
-    }
-}
-
-//OPEN AND CLOSE BROWSER TAB
-
+//OPEN CLOSE & TOGGLE BROWSER CONTAINER/TABS
+const browserApp = document.querySelector('.browser-container');
+const browserHeader = document.querySelector('.browser-header');
 const browserTab = document.getElementById('browser');
 
+const paintApp = document.querySelector('.paint-container');
+const paintHeader = document.querySelector('.paint-header');
+const paintTab = document.getElementById('paint');
+
+function openBrowser() {
+  browserApp.classList.remove('hidden');
+  browserApp.style.zIndex = 1;
+  paintApp.style.zIndex = 0;
+}
+
 function showBrowserTab() {
-    browserTab.classList.remove('hidden');
-    browserTab.classList.add('show');
+  browserTab.classList.remove('hidden');
+  browserTab.classList.add('show');
+}
+
+function closeBrowser() {
+  browserApp.classList.add('hidden');
+  closeBrowserTab();
 }
 
 function closeBrowserTab() {
-    if(browserTab.classList.contains('show')) {
-        browserTab.classList.remove('show');
-        browserTab.classList.add('hidden'); 
-    }
+  if (browserTab.classList.contains('show')) {
+    browserTab.classList.remove('show');
+    browserTab.classList.add('hidden');
+  }
 }
 
-// DRAG FUNCTION FOR APPS
-// Get the element that we want to make draggable
-const appContainer = document.querySelector('.app-container');
-const appHeader = document.querySelector('.app-header');
+function toggleBrowser() {
+  if (browserTab.classList.contains('show')) {
+    browserApp.classList.toggle('hidden');
+    if(paintApp.style.zIndex !== 0) {
+      paintApp.style.zIndex = 0;
+      browserApp.style.zIndex = 1;
+    }
+  }
+}
+
+// OPEN  CLOSE & TOGGLE PAINT APP CONTAINER/TAB
+
+function openPaint() {
+  paintApp.classList.remove('hidden');
+  paintApp.style.zIndex = 1;
+  browserApp.style.zIndex = 0;
+}
+
+function showPaintTab() {
+  paintTab.classList.remove('hidden');
+  paintTab.classList.add('show');
+}
+
+function closePaint() {
+  paintApp.classList.add('hidden');
+  closePaintTab();
+}
+
+function closePaintTab() {
+  if (paintTab.classList.contains('show')) {
+    paintTab.classList.remove('show');
+    paintTab.classList.add('hidden');
+  }
+}
+
+function togglePaint() {
+  if (paintTab.classList.contains('show')) {
+    paintApp.classList.toggle('hidden');
+    if(browserApp.style.zIndex !== 0) {
+      browserApp.style.zIndex = 0;
+      paintApp.style.zIndex = 1;
+    }
+  }
+}
+
+// DRAG FUNCTION FOR BROWSER
 
 // Set initial values for position tracking
-let isDragging = false;
+let webDragging = false;
 let dragStartX;
 let dragStartY;
 let initialX;
 let initialY;
 
 // When the user clicks on the header, start dragging
-appHeader.addEventListener('mousedown', function(e) {
-  isDragging = true;
-
+browserHeader.addEventListener('mousedown', function(e) {
+  webDragging = true;
+  browserApp.style.zIndex = 1;
+  paintApp.style.zIndex = 0;
   // Record the starting position
   dragStartX = e.clientX;
   dragStartY = e.clientY;
 
   // Record the initial position of the appContainer
-  const computedStyle = window.getComputedStyle(appContainer);
+  const computedStyle = window.getComputedStyle(browserApp);
   initialX = parseInt(computedStyle.getPropertyValue('left'));
   initialY = parseInt(computedStyle.getPropertyValue('top'));
 
@@ -62,12 +104,12 @@ appHeader.addEventListener('mousedown', function(e) {
 
 // When the user releases the mouse button, stop dragging
 document.addEventListener('mouseup', function(e) {
-  isDragging = false;
+  webDragging = false;
 });
 
 // When the user moves the mouse, update the position of the appContainer if we're currently dragging
 document.addEventListener('mousemove', function(e) {
-  if (isDragging) {
+  if (webDragging) {
     // Calculate the new position
     const dragX = e.clientX - dragStartX;
     const dragY = e.clientY - dragStartY;
@@ -75,7 +117,49 @@ document.addEventListener('mousemove', function(e) {
     const newY = initialY + dragY;
 
     // Set the new position of the appContainer
-    appContainer.style.left = newX + 'px';
-    appContainer.style.top = newY + 'px';
+    browserApp.style.left = newX + 'px';
+    browserApp.style.top = newY + 'px';
+  }
+});
+
+//DRAG FUNCTION FOR PAINT | USES SAME INITIAL POS TRACKING VARIABLES
+let paintDragging = false;
+
+// When the user clicks on the header, start dragging
+paintHeader.addEventListener('mousedown', function(e) {
+  paintDragging = true;
+
+  paintApp.style.zIndex = 1;
+  browserApp.style.zIndex = 0;
+  // Record the starting position
+  dragStartX = e.clientX;
+  dragStartY = e.clientY;
+
+  // Record the initial position of the appContainer
+  const computedStyle = window.getComputedStyle(paintApp);
+  initialX = parseInt(computedStyle.getPropertyValue('left'));
+  initialY = parseInt(computedStyle.getPropertyValue('top'));
+
+  // Prevent text selection during drag
+  e.preventDefault();
+});
+
+// When the user releases the mouse button, stop dragging
+document.addEventListener('mouseup', function(e) {
+  paintDragging = false;
+});
+
+// When the user moves the mouse, update the position of the appContainer if we're currently dragging
+document.addEventListener('mousemove', function(e) {
+  if (paintDragging) {
+    // Calculate the new position
+    const dragX = e.clientX - dragStartX;
+    const dragY = e.clientY - dragStartY;
+    const newX = initialX + dragX;
+    const newY = initialY + dragY;
+
+    // Set the new position of the appContainer
+    paintApp.style.left = newX + 'px';
+    paintApp.style.top = newY + 'px';
   }
 });
